fec10c1e42e8a46582f3355af3e5f34d9071fda3
gogarcia <15249923+gogarcia@users.noreply.github.com>
2021-08-21 22:10:39 +0200

    cypher works

--- caller/cypher.py
+++ caller/cypher.py
@@ -3,6 +3,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto import Random
 import config
+import base64
 
 """
     Protocolo de seguridad:
@@ -17,79 +18,55 @@ import config
 class Cyp():
     def __init__(self):
         self._rsa_key = RSA.generate(config._RSA_BITS, Random.new().read)
-        self._cypher = None
+        self._key = None
     
     @property
     def rsa_publickey(self):
         return self._rsa_key.public_key().export_key(format='PEM')
 
-    def encrypt_cypher(self, public_key):
+    def create_and_encrypt_key(self, public_key):
         import os
-        if self._cypher:
-            raise Exception("AES key was already created or imported")
-
-        key = os.urandom(int(config._AES_BITS/8))
-        self._cypher = AES.new(key, AES.MODE_ECB)
+        self._key = os.urandom(int(config._AES_BITS/8))
         recipient_key = RSA.importKey(public_key)
         cypher = PKCS1_OAEP.new(recipient_key)
-        return cypher.encrypt(key)
+        return cypher.encrypt(self._key)
     
-    def decrypt_cypher(self, key):
-        if self._cypher:
-            raise Exception("AES key was already created or imported")
+    def decrypt_and_save_key(self, key):
         recipient_key = self._rsa_key
         cypher = PKCS1_OAEP.new(recipient_key)
-        key = cypher.decrypt(key)
-        self._cypher = AES.new(key, AES.MODE_ECB)
+        self._key = cypher.decrypt(key)
 
     def encrypt(self, data):
-        assert self._cypher, ("No AES key")
-        return self._cypher.encrypt(data)
+        assert self._key, ("No AES key")
+        data = self._pad(data)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self._key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(data.encode()))
 
     def decrypt(self, data):
-        assert self._cypher, ("No AES key")
-        data = self._cypher.decrypt(data)
-        return data
+        data = base64.b64decode(data)
+        iv = data[:AES.block_size]
+        cipher = AES.new(self._key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(data[AES.block_size:])).decode('utf-8')
+    
+    def _pad(self, data):
+        block_size = AES.block_size
+        return data + (block_size - len(data) % block_size) * chr(block_size - len(data) % block_size)
 
-def _TO_BYTES(object):
-    import pickle
-    return pickle.dumps(object)
-
-def _FROM_BYTES(data):
-    import pickle
-    return pickle.loads(data)
+    def _unpad(self, data):
+        return data[:-ord(data[len(data)-1:])]
 
 if __name__ == '__main__':
-    class a():
-        def __init__(self, a, b=None):
-            self._a = a
-            self._b = b
-            self._c = "c"
-            self._d = [1, 2, 3]
-            self._e = {"1":1, "2":2}
-        
-        def pr(self):
-            print("Printin a object:")
-            print("   >>> a: {}".format(self._a))
-            print("   >>> b: {}".format(self._b))
-            print("   >>> c: {}".format(self._c))
-            print("   >>> d: {}".format(self._d))
-            print("   >>> e: {}".format(self._e))
-
-    a = a("a")
-    b = "Simple string"
-    c = [1, 2, 3, 4]
-    d = {"1": 1, "2": 2}
+    a = "Simple string"
 
     server = Cyp()
     client = Cyp()
 
     client_public = client.rsa_publickey  # Client creates RSA public  key
-    server_secret_e = server.encrypt_cypher(client_public)  # Server creates AES key and sends it encrypted
-    client.decrypt_cypher(server_secret_e)  # Client decrypts AES key and stores
+    server_secret_e = server.create_and_encrypt_key(client_public)  # Server creates AES key and sends it encrypted
+    client.decrypt_and_save_key(server_secret_e)  # Client decrypts AES key and stores
 
-    b_e = client.encrypt(b)
-    print(str(b_e))
-    b_d = server.decrypt(b_e)
-    print(b_d)
-
+    a_e = client.encrypt(a)
+    print(str(a_e))
+    a_d = server.decrypt(a_e)
+    print(a_d)
--- caller/message.py
+++ caller/message.py
@@ -1,7 +1,7 @@
 class Message():
     """Message class, this is what would be sended
     """
-    def __init__(self, data, callback=False, status="OK"):
+    def __init__(self, data=None, callback=None, args=None, kargs=None, status="OK"):
         """Constructor of message class
 
         Args:
@@ -12,7 +12,8 @@ class Message():
         from datetime import datetime
         self._data = data
         self._callback = callback
-        self._classname = data.__class__.__name__
+        self._args = args
+        self._kargs = kargs
         self._status = status
         self._time = datetime.now()
     
@@ -25,13 +26,44 @@ class Message():
         return self._callback
     
     @property
-    def classname(self):
-        return self._classname
-    
-    @property
     def status(self):
         return self._status
     
     @property
+    def args(self):
+        return self._args
+    
+    @property
+    def kargs(self):
+        return self._kargs
+    
+    @property
     def time(self):
         return self._time
+
+    @property
+    def __dict__(self):
+        import ast
+        ast.literal_eval(self.__dict__())
+        return {
+            "data": self._data,
+            "callback": self._callback,
+            "args": str. join(self._args, "//////"), # This is a tuple, we need to join it
+            "kargs": ast.literal_eval(self._kargs), # This is a dict, we need to create a string
+            "status": self._status,
+            "time": self._time.strftime("%m/%d/%Y-%H:%M:%S") # This is datetime, we need a string
+        }
+    
+    @property
+    def __str__(self):
+        import ast
+        return ast.literal_eval(self.__dict__())
+    
+    @staticmethod
+    def from_dict(dict):
+        import datetime
+        data = dict.get('data')
+        callback = dict.get('callback')
+        args = dict.get('args')
+        time = datetime.strptime(dict.time, "%m/%d/%Y-%H:%M:%S")
+
